94c5bc9de224aad9e2ec958af4e461ef279a3772
ddavid09 <ddawidziak.dd@gmail.com>
2020-01-14 00:09:04 +0100

    Correct loading sim parameters out of the loadWindow

--- src/pl/dawidziak/model/Environment.java
+++ src/pl/dawidziak/model/Environment.java
@@ -13,16 +13,16 @@ public class Environment extends BasicSimObj {
 
     private void log(){
         System.out.println("Utworzono Srodowisko symulacji");
-        System.out.println("Limit klientow: \t\t" + simParameters.clientAmount);
-        System.out.println("Liczba stanowisk: \t\t" + simParameters.postAmount);
-        System.out.println("Rozmiar kolejki: \t\t" + simParameters.postQueueSize);
-        System.out.println("Liczba kas: \t\t\t" + simParameters.counterAmount);
+        System.out.println("Limit klientow: \t\t\t\t" + simParameters.clientAmount);
+        System.out.println("Liczba stanowisk: \t\t\t\t" + simParameters.postAmount);
+        System.out.println("Rozmiar kolejki: \t\t\t\t" + simParameters.postQueueSize);
+        System.out.println("Liczba kas: \t\t\t\t\t" + simParameters.counterAmount);
         System.out.println("Rozklad prawdop. tworzenia klientow: \t\t" + simParameters.clientDistrib.toString());
-        System.out.println("Rozklad prawdop. losowania PB: \t\t" + simParameters.PBtankTimeDistrib.toString());
-        System.out.println("Rozklad prawdop. losowania ON: \t\t" + simParameters.ONtankTimeDistrib.toString());
-        System.out.println("Rozklad prawdop. losowania LPG: \t\t" + simParameters.LPGtankTimeDistrib.toString());
-        System.out.println("Rozklad prawdop. losowania myjni \t\t" + simParameters.carWashChoiceDistrib.toString());
-        System.out.println("Rozklad prawdop. wyboru paliwa: \t\t" + simParameters.fuelChoiceDistrib.toString());
+        System.out.println("Rozklad prawdop. losowania PB: \t\t\t\t" + simParameters.PBtankTimeDistrib.toString());
+        System.out.println("Rozklad prawdop. losowania ON: \t\t\t\t" + simParameters.ONtankTimeDistrib.toString());
+        System.out.println("Rozklad prawdop. losowania LPG: \t\t\t" + simParameters.LPGtankTimeDistrib.toString());
+        System.out.println("Rozklad prawdop. losowania myjni \t\t\t" + simParameters.carWashChoiceDistrib.toString());
+        System.out.println("Rozklad prawdop. wyboru paliwa: \t\t\t" + simParameters.fuelChoiceDistrib.toString());
 
     }
 }
--- src/pl/dawidziak/view/LoadParametersController.java
+++ src/pl/dawidziak/view/LoadParametersController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 import pl.dawidziak.model.Distribution;
 import pl.dawidziak.model.DistributionName;
 import pl.dawidziak.model.Environment;
@@ -68,13 +69,51 @@ public class LoadParametersController implements Initializable {
     }
 
     public void startButtonOnAction(ActionEvent actionEvent) {
+
+        DistribParameters = new ArrayList<>();
+
+        for(HBox hbox : paramsHBoxes){
+            int paramsAmount = hbox.getChildren().size();
+            Number[] params = new Number[paramsAmount];
+            var array = hbox.getChildren();
+            if(paramsAmount == 1){
+                TextField tf = (TextField)array.get(0);
+                params[0] = Double.parseDouble(tf.getText());
+            }else if(paramsAmount == 2){
+                TextField tf1 = (TextField)array.get(0);
+                TextField tf2 = (TextField)array.get(1);
+                params[0] = Double.parseDouble(tf1.getText());
+                params[1] = Double.parseDouble(tf2.getText());
+            }else{
+                TextField tf1 = (TextField)array.get(0);
+                TextField tf2 = (TextField)array.get(1);
+                TextField tf3 = (TextField)array.get(2);
+                params[0] = Double.parseDouble(tf1.getText());
+                params[1] = Double.parseDouble(tf2.getText());
+                params[2] = Double.parseDouble(tf3.getText());
+            }
+            DistribParameters.add(params);
+        }
+
         int clientAmount = Integer.parseInt(clientAmountInput.getText());
         int postAmount = Integer.parseInt(postAmountInput.getText());
         int postQueueSize = Integer.parseInt(postQueueSizeInput.getText());
         int counterAmount = Integer.parseInt(counterAmountInput.getText());
+        //Distributions
+        Distribution clientDistrib = new Distribution(clientDistribComboBox.getValue(), DistribParameters.get(0));
+        Distribution PBtankTimeDistrib = new Distribution(PBtimeDistribComboBox.getValue(), DistribParameters.get(1));
+        Distribution ONtankTimeDistrib = new Distribution(ONtimeDistribComboBox.getValue(), DistribParameters.get(2));
+        Distribution LPGtankTimeDistrib = new Distribution(LPGtimeDistribComboBox.getValue(), DistribParameters.get(3));
+        Distribution carWashChoiceDistrib = new Distribution(washDistribComboBox.getValue(), DistribParameters.get(4));
+        Distribution fuelChoiceDistrib = new Distribution(fuelTypeDistribComboBox.getValue(), DistribParameters.get(5));
 
+        //SimParameters
+        var simParams = new SimParameters(clientAmount, postAmount, postQueueSize, counterAmount, clientDistrib, fuelChoiceDistrib, PBtankTimeDistrib, LPGtankTimeDistrib, ONtankTimeDistrib, carWashChoiceDistrib);
+        //Environment
+        new Environment(simParams);
 
-
+        Stage stage = (Stage) gridLayout.getScene().getWindow();
+        stage.close();
     }
 
     public void distributionOnChange(ActionEvent actionEvent) {
